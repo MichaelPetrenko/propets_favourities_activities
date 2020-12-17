b7aabae512328ee30d3fbea43d354bb9d0fbe31a
Severa3 <reizmp4@gmail.com>
2020-12-17 20:46:37 +0200

    wakeUp

--- ProPets_favorites_activities_service/src/telran/favorites/controllers/FavoritesController.java
+++ ProPets_favorites_activities_service/src/telran/favorites/controllers/FavoritesController.java
@@ -31,6 +31,12 @@ public class FavoritesController {
 		String xToken = request.getHeader("X-Token");
 		return favoritesService.getActivityLostFoundPosts(login, service, xToken);
 	}
+	
+	// "/wakeup"
+	@GetMapping(value = FavoritesApiConstants.WAKEUP)
+	void wakeUp() {
+		return;
+	}
 
 	// HERE WILL BE HOTELS GETTER
 
